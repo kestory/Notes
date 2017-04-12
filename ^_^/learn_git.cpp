c04d0b03760d889aaca7e2479a687351d90ce2b7
Kestory <kestory@163.com>
2017-04-12 19:26:45 +0800

    git2github,readme ok more knowledge

--- ^_^/learn_git.cpp
+++ ^_^/learn_git.cpp
@@ -31,18 +31,30 @@ star则类似于Facebook和Twitter上的”like
 fork就是将别人的项目拷贝一份给自己，new一个新的分支(Branch)出来，你push的改进和补充则又由原作者选择是否接受。
 当要push代码到git时，出现提示：
 	error:failed to push some refs to ...
-出现原因在于：git仓库中已经有一部分代码，所以它不允许你直接把你的代码覆盖上去
-可以强推，即利用强覆盖方式用你本地的代码替代git仓库内的内容
+reason: git仓库中有新内容(README)而本地没有
+//GitHub repo has seen new commits pushed to it
+可以强推，即强覆盖
 git push -f
+接受新内容就:
+git pull --rebase origin master
+git push origin master
 
 ssh -T git@github.com
 验证ssh配置是否成功
 
-
-关联一个远程库git remote add <name> git@git@github.com:kestory/Notes.git 	//注意有冒号":" <name>是远程库名字
-关联后，使用命令git push -u <name> master第一次推送master分支的所有内容
-以后git push <name> master推送最新修改
-
+------------
+|  remote  |
+------------
+git remote -v
+列出所有远程仓库信息, 包括网址.
 git remote rm origin//删除名字叫origin的远程库
 进入.git文件夹cat config可以看到有哪些远程库
+git remote rename hi hello
+重命名本地远程仓库名, 从hi改为hello
+
+关联一个远程库git remote add <name> git@git@github.com:kestory/Notes.git 	//注意有冒号":" <name>是远程库名字
+关联后，使用命令git push -u <name> master第一次推送master分支的所有内容，以后git push 即可
+以后git push <name> master推送最新修改
+
+
 
