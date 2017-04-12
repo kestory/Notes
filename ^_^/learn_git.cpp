c48ad9c13ea3fa5357c6894a9b6a26cf07c1f83c
Kestory <kestory@163.com>
2017-04-13 00:03:24 +0800

    git2hub bug fix

--- ^_^/learn_git.cpp
+++ ^_^/learn_git.cpp
@@ -29,6 +29,7 @@ git reset --hard bab6864
 Watch也就是关注该repo的动态
 star则类似于Facebook和Twitter上的”like
 fork就是将别人的项目拷贝一份给自己，new一个新的分支(Branch)出来，你push的改进和补充则又由原作者选择是否接受。
+
 当要push代码到git时，出现提示：
 	error:failed to push some refs to ...
 reason: git仓库中有新内容(README)而本地没有
@@ -38,6 +39,9 @@ git push -f
 接受新内容就:
 git pull --rebase origin master
 git push origin master
+
+error: src refspec master does not match any.
+reason:目录中没有文件，空目录是不能提交上去的(忘了git add了)
 
 ssh -T git@github.com
 验证ssh配置是否成功
@@ -52,7 +56,7 @@ git remote rm origin//删除名字叫origin的远程库
 git remote rename hi hello
 重命名本地远程仓库名, 从hi改为hello
 
-关联一个远程库git remote add <name> git@git@github.com:kestory/Notes.git 	//注意有冒号":" <name>是远程库名字
+关联一个远程库git remote add <name> git@github.com:kestory/Notes.git 	//注意有冒号":" <name>是远程库名字
 关联后，使用命令git push -u <name> master第一次推送master分支的所有内容，以后git push 即可
 以后git push <name> master推送最新修改
 
