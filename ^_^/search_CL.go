e42ad9817800ab0c70db641e9bbc82b7fc9e9f6a
Kestory <kestory@163.com>
2017-04-17 18:47:12 +0800

    add search_CL

---
+++ ^_^/search_CL.go
@@ -0,0 +1,30 @@
+https://www.zhihu.com/question/20161362
+https://sspai.com/post/34150
+双引号 完全匹配搜索
+减号 减号前面必须是空格，减号后面紧跟着需要排除的词
+	南京大学 -研究生
+	苹果 -手机
+星号*是常用的通配符
+	搜索*擎
+	许*谦
+intitle: 指令返回的是页面 title 中包含关键词的页面//in&&all
+allintitle:搜索返回的是页面标题中包含多组关键词的文件。
+	allintitle:SEO 搜索引擎优化
+	==intitle:SEO intitle:搜索引擎优化
+
+inurl: 指令用于搜索查询词出现在 url 中的页面。返回的结果都是网址url 中包含“搜索引擎优化”的页面
+	inurl:搜索引擎优化
+	inurl:github kestory//通过这个命令我找到了我以前删掉的rep的连接，然后打开404 not found哈哈
+allinurl:SEO 搜索引擎优化
+	==inurl:SEO inurl:搜索引擎优化
+
+inanchor:指令返回的结果是导入链接锚文字中包含搜索词的页面。
+	inanchor:点击这里//返回的结果页面本身并不一定包含“点击这里”这四个字
+	//而是指向这些页面的链接锚文字中出现了“点击这里”这四个字。可以用来找到某个关键词的竞争对收
+related:返回与某个网站有关联的页面
+	related:http://cnseotool.com
+	//得到与点石网站有关联的其他页面。 这种关联一般认为指的是有共同外部链接的网站。
+filetype:pdf
+site:www.zhihu.com
+
+
