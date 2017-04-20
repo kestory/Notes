83e4bf08872d641a020ee2294c1cbd3ef6f218a6
Kestory <kestory@163.com>
2017-04-20 10:02:47 +0800

    tiny changes

---
+++ STL学习/string.cpp
@@ -0,0 +1,5 @@
+str.size() and str.length() are just synonyms.
+size() is there to be consistent with other STL containers (like vector, map, etc.),
+length() is there to make things more readable.
+str.empty();//检查 str 是否为空，为空返回 1，否则返回 0
+str[n];
