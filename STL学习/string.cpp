f033629efbef7efe77b4acdaacf2d64f7dbc9b90
Kestory <kestory@163.com>
2017-04-20 10:05:00 +0800

    add string.cpp

--- STL学习/string.cpp
+++ STL学习/string.cpp
@@ -1,5 +1,25 @@
 str.size() and str.length() are just synonyms.
 size() is there to be consistent with other STL containers (like vector, map, etc.),
 length() is there to make things more readable.
+// comparing size, length, capacity and max_size
+#include <iostream>
+#include <string>
+int main ()
+{
+  std::string str ("Test string");
+  std::cout << "size: " << str.size() << "\n";
+  std::cout << "length: " << str.length() << "\n";
+  std::cout << "capacity: " << str.capacity() << "\n";
+  std::cout << "max_size: " << str.max_size() << "\n";
+  return 0;
+}
+
+A possible output for this program could be:
+size: 11
+length: 11
+capacity: 15
+max_size: 4294967291
+
+
 str.empty();//检查 str 是否为空，为空返回 1，否则返回 0
 str[n];
