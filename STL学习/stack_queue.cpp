be8d09e35fa0e62d74aa82e425dfca206331c190
Kestory <kestory@163.com>
2017-04-21 19:35:13 +0800

    tiny changes

--- STL学习/stack_queue.cpp
+++ STL学习/stack_queue.cpp
@@ -15,3 +15,39 @@ q.pop()		删除队列首元素但不返回其值
 q.front()	返回队首元素的值，但不删除该元素
 q.push()	在队尾压入新元素
 q.back()	返回队列尾元素的值，但不删除该元素
+
+电话号码要输字符串时
+#include <iostream>
+#include <stack>
+using namespace std;
+
+struct elem { char name[20]; };
+
+istream &operator >> (istream &in, elem &e)
+{ return in >> e.name; }
+
+ostream &operator << (ostream &out, const elem &e)
+{ return out << e.name; }
+
+int main(int argc, char* argv[])
+{
+	elem e;
+	stack<elem> s;
+
+	for (int i = 0; i < 3; i++) {
+		cin >> e;
+		s.push(e);
+	}
+
+	for (; !s.empty(); s.pop())
+		cout << s.top() << endl;
+
+	return 0;
+}
+------------------------------------
+Tom    # 输入
+Bob    # 输入
+Jerray    # 输入
+Jerray
+Bob
+Tom
