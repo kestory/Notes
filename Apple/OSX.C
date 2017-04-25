4b1a5f17272424ea8dc5922f32e3ad0f9e8f1897
Kestory <kestory@163.com>
2017-04-25 09:53:23 +0800

    add dir Apple

---
+++ Apple/OSX.C
@@ -0,0 +1,20 @@
+Homebrew
+搜索软件：brew search 软件名，如brew search wget
+安装软件：brew install 软件名，如brew install wget
+卸载软件：brew remove 软件名，如brew remove wget
+
+
+"MacTips"
+调整 Launchpad 布局
+defaults write com.apple.dock springboard-columns -int 5;
+defaults write com.apple.dock springboard-rows -int 3;
+defaults write com.apple.dock ResetLaunchPad -bool TRUE;
+killall Dock
+
+command + i 可以打开简介 (command + o 打开文件or目录)，在简介的注释里添加关键词以后可以用Spotlight搜索
+
+Safari浏览到网页想发给别人，直接command + i就会打开邮件并把网页附加进去
+
+系统软件输入英文时按esc键(或者 fn + f5 )会显示可能单词
+
+想改截图默认的png到jpeg：defaults write com.apple.screencapture type -string JPEG
