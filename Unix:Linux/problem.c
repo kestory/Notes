0ba3237c67dc8dac3926ef5338f46876d9e36e87
Kestory <kestory@163.com>
2017-04-25 10:28:33 +0800

    tiny changes

--- Unix:Linux/problem.c
+++ Unix:Linux/problem.c
@@ -1,7 +1,9 @@
-使用apt-get命令安装所需的开发软件非常方便，但也有不少时候由于链接不上软件源而不能正常安装
-于是很多人都会想到使用sudo apt-get update命令来更新软件源
-而恰恰这时屋漏偏逢连夜雨，更新命令也不能正常工作，出现大量“Failed to fetch”的错误。
-设置妥当DNS服务器即可
+apt-get连接不上软件源
+sudo apt-get update命令来更新软件源
+如果屋漏偏逢连夜雨，更新命令也不能正常工作，大量"Failed to fetch"
+换源 设置妥当DNS服务器即可
 sudo vi /etc/resolv.conf
 //add the line
 nameserver 8.8.8.8
+
+打开software商店，edit里面china点Chose Best Server
