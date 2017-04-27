ce92ba325e8cef06c60cd92a619ebc9f966cac83
Kestory <kestory@163.com>
2017-04-27 22:28:29 +0800

    tiny changes

---
+++ C:CPP/class.cpp
@@ -0,0 +1,25 @@
+/构造函数调用顺序:基类->成员对象->本身/
+#include <iostream>
+using namespace std;
+class A{
+public:
+	A()	{cout<<"A"<<endl;}
+};
+class C{
+public:
+	C()	{cout<<"C"<<endl;}
+};
+class B: public A{
+public:
+	B()	{cout<<"B"<<endl;}
+	C c;
+};
+int main(int argc, char const *argv[]){
+	B *b=new B();
+	return 0;
+}
+64位g++下，输出为
+A
+C
+B
+
