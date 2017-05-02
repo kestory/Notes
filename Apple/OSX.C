0ce5927a285e6361c68df7a5cf560fe74c033b41
Kestory <kestory@163.com>
2017-05-02 16:50:35 +0800

    tiny changes

--- Apple/OSX.C
+++ Apple/OSX.C
@@ -2,6 +2,7 @@ Homebrew
 搜索软件：brew search 软件名，如brew search wget
 安装软件：brew install 软件名，如brew install wget
 卸载软件：brew remove 软件名，如brew remove wget
+查看信息：brew info 软件名，如brew info wget
 
 
 "MacTips"
@@ -11,10 +12,11 @@ defaults write com.apple.dock springboard-rows -int 3;
 defaults write com.apple.dock ResetLaunchPad -bool TRUE;
 killall Dock
 
-command + i 可以打开简介 (command + o 打开文件or目录)，在简介的注释里添加关键词以后可以用Spotlight搜索
+command + i 可以打开简介 (command + o 打开文件or目录)
+在简介的注释里添加关键词以后可以用Spotlight搜索
 
-Safari浏览到网页想发给别人，直接command + i就会打开邮件并把网页附加进去
+Safari 浏览到网页想发给别人，直接command + i就会打开邮件并把网页附加进去
 
 系统软件输入英文时按esc键(或者 fn + f5 )会显示可能单词
 
-想改截图默认的png到jpeg：defaults write com.apple.screencapture type -string JPEG
+想改截图默认的 png 到 jpeg：defaults write com.apple.screencapture type -string JPEG
--- Apple/Wireshark.C
+++ Apple/Wireshark.C
@@ -1,1 +1,18 @@
-列可以排序，可以移动
+点击选中一个包，点击右键选择Follow--TCP Stream，可查看包的详细内容
+
+IP过滤（如：ip.addr == x.x.x.x，ip.src == x.x.x.x, ip.dst == x.x.x.x）
+协议过滤（如：HTTP、HTTPS、SMTP、ARP等）
+端口过滤（如：tcp.port == 21、udp.port == 53）
+组合过滤（如：ip.addr == x.x.x.x && tcp.port == 21、tcp.port == 21 or udp.port == 53）
+
+
+
+
+TCP抓包分析
+用 tcp.flags.syn == 1 过滤三次握手的包
+用 tcp.flags.fin == 1 过滤得四次断开的包 //TCP包中Info列标志位为[FIN,ACK]的包是TCP四次断开的包
+
+通过高亮显示颜色来推断出一个完整的会话//高亮显示三次握手和四次断开
+选好某帧	右击->对话着色(Colorize Conversation)->TCP->Color1
+
+为了方便分析可以File->Export Specified Packets(导出特定分组...)然后在Range: 70 - 90就会导出你要的那块
