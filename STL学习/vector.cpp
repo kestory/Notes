f0d255303717d60951280ccaf26b737f93699421
Kestory <kestory@163.com>
2017-05-22 20:05:58 +0800

    tiny changes

--- STL学习/vector.cpp
+++ STL学习/vector.cpp
@@ -6,36 +6,39 @@ vector<int> b(a);	//声明并用向量a初始化向量b
 vector<int> b(a.begin(), a.begin() + 3);	//将a向量中从第0个到第2个(共3个)作为向量b的初始值
 用数组来初始化向量
 int n[] = {1, 2, 3, 4, 5};
-vector<int> a(n, n+5);              //将数组n的前5个元素作为向量a的初值
+vector<int> a(n, n + 5);            //将数组n的前5个元素作为向量a的初值
 vector<int> a(&n[1], &n[4]);        //将n[1] - n[4]范围内的元素作为向量a的初值
 
-a.size();//the number of elements, which is the number of actual objects held in the vector, not necessarily equal to its storage capacity
+
 
 
 int max = *max_element(v.begin(), v.end());
 
-// vector::size
+/
+.size();//the number of elements, which is the number of actual objects held in the vector, not necessarily equal to its storage capacity
+.insert(iterator position, size_type n, const value_type& val);
+lambda
+/
 #include <iostream>
 #include <vector>
-#define D(x) cout<<#x<<"="<<x<<"\t";
 using namespace std;
 int main ()
 {
-    vector<int> myints;
-    cout << "0. size: " << myints.size() << '\n';
-    for_each(myints.begin(),myints.end(),[](int x){D(x)});
+	vector<int> myints;
+	cout << "0. size: " << myints.size() << '\n';
+	for_each(myints.begin(), myints.end(), [](int x) {cout << x << '\t';});
 
-    for (int i=0; i<10; i++) myints.push_back(i);
-    cout << "\n1. size: " << myints.size() << '\n';
-    for_each(myints.begin(),myints.end(),[](int x){D(x)});
+	for (int i = 0; i < 10; i++) myints.push_back(i);
+	cout << "\n1. size: " << myints.size() << '\n';
+	for_each(myints.begin(), myints.end(), [](int x) {cout << x << '\t';});
 
-    myints.insert (myints.end(),10,100);
-    cout << "\n2. size: " << myints.size() << '\n';
-    for_each(myints.begin(),myints.end(),[](int x){D(x)});
+	myints.insert (myints.end(), 10, 100);
+	cout << "\n2. size: " << myints.size() << '\n';
+	for_each(myints.begin(), myints.end(), [](int x) {cout << x << '\t';});
 
-    myints.pop_back();
-    cout << "\n3. size: " << myints.size() << '\n';
-    for_each(myints.begin(),myints.end(),[](int x){D(x)});
+	myints.pop_back();
+	cout << "\n3. size: " << myints.size() << '\n';
+	for_each(myints.begin(), myints.end(), [](int x) {cout << x << '\t';});
 
-    return 0;
+	return 0;
 }
