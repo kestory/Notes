4b6771fbb515de0ac37f63aac1b5b004217be1bb
Kestory <kestory@163.com>
2017-05-22 20:02:06 +0800

    tiny changes

--- STL学习/vector.cpp
+++ STL学习/vector.cpp
@@ -9,4 +9,7 @@ int n[] = {1, 2, 3, 4, 5};
 vector<int> a(n, n+5);              //将数组n的前5个元素作为向量a的初值
 vector<int> a(&n[1], &n[4]);        //将n[1] - n[4]范围内的元素作为向量a的初值
 
+a.size();//the number of elements, which is the number of actual objects held in the vector, not necessarily equal to its storage capacity
+
+
 int max = *max_element(v.begin(), v.end());
