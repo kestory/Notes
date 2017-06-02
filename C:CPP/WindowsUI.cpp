932dd33742e09e2323d7816c16d74c30b94a201f
Kestory <kestory@163.com>
2017-06-02 17:16:40 +0800

    tiny changes

--- C:CPP/WindowsUI.cpp
+++ C:CPP/WindowsUI.cpp
@@ -1,5 +1,5 @@
 使用Windows API编程//new project -> win32 project
 case WM_LBUTTONDOWN:
 MessageBox(hWnd, _T("I'm Saying\nMu ha ha"), _T("Caption"), MB_ICONEXCLAMATION | MB_YESNOCANCEL | MB_DEFBUTTON2);
-//一个惊叹号三个选项默认放在第二个选项
+//一个惊叹号 | 三个选项 | 默认放在第二个选项
 break;
