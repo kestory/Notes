d1ac3f7abfb7025318efabce3d33766ecf79346e
Kestory <kestory@163.com>
2017-06-02 19:59:03 +0800

    tiny changes

--- C:CPP/Windows.cpp
+++ C:CPP/Windows.cpp
@@ -3,3 +3,16 @@ case WM_LBUTTONDOWN:
 MessageBox(hWnd, _T("I'm Saying\nMu ha ha"), _T("Caption"), MB_ICONEXCLAMATION | MB_YESNOCANCEL | MB_DEFBUTTON2);
 //一个惊叹号 | 三个选项 | 默认放在第二个选项
 break;
+
+
+
+
+
+
+typedef struct _COORD {
+  SHORT X;
+  SHORT Y;
+} COORD, *PCOORD;
+COORD结构体
+COORD pos;
+它的两个成员是X和Y	(0,0) is at the top, left cell of the buffer.
