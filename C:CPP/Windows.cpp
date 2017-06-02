970f306b59e5eaa9293ae397ae1c782eee08ad5e
Kestory <kestory@163.com>
2017-06-02 21:20:42 +0800

    tiny changes

--- C:CPP/Windows.cpp
+++ C:CPP/Windows.cpp
@@ -1,3 +1,4 @@
+-------COORD struct-------
 typedef struct _COORD {
   SHORT X;
   SHORT Y;
@@ -8,17 +9,19 @@ COORD pos;
 (0,0) is at the top, left cell of the buffer.
 
 
-typedef void *HANDLE;//HANDLE == void*
+-------HANDLE == void*-------
+typedef void *HANDLE;//
 
 
-
+-------GetStdHandle(STD_OUTPUT_HANDLE)-------
 HANDLE GetStdHandle( DWORD nStdHandle );
 从一个特定的标准设备（标准输入、标准输出或标准错误）中取得一个句柄（用来标识不同设备的数值）。可以嵌套使用。
 //Retrieves a handle to the specified standard device (standard input, standard output, or standard error).
-nStdHandle ~~ The standard device. This parameter can be one of the following values.
-STD_INPUT_HANDLE	//标准输入的句柄
-STD_OUTPUT_HANDLE	//标准输出的句柄
-STD_ERROR_HANDLE	//标准错误的句柄
+nStdHandle	The standard device. This parameter can be one of the following values.
+	STD_INPUT_HANDLE	//标准输入的句柄
+	STD_OUTPUT_HANDLE	//标准输出的句柄
+	STD_ERROR_HANDLE	//标准错误的句柄
+
 
 == 彩色的Hello World! ==
 #include <stdio.h>
