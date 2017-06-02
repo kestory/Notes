9341c19b4ddfb2750d4ade974f366eada69db7be
Kestory <kestory@163.com>
2017-06-02 20:21:07 +0800

    tiny changes

--- C:CPP/Windows.cpp
+++ C:CPP/Windows.cpp
@@ -1,11 +1,3 @@
-使用Windows API编程//new project -> win32 project
-case WM_LBUTTONDOWN:
-	MessageBox(hWnd, _T("I'm Saying\nMu ha ha"), _T("Caption"), MB_ICONEXCLAMATION | MB_YESNOCANCEL | MB_DEFBUTTON2);
-	//一个惊叹号 | 三个选项 | 默认放在第二个选项
-break;
-
-
-
 typedef struct _COORD {
   SHORT X;
   SHORT Y;
