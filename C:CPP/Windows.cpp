bc3475d7c073069fe5abd1b428f6f711b3ceae9d
Kestory <kestory@163.com>
2017-06-03 12:50:54 +0800

    tiny changes

--- C:CPP/Windows.cpp
+++ C:CPP/Windows.cpp
@@ -59,16 +59,18 @@ SetConsoleTextAttribute(hConsole, 7);
 BOOL SetConsoleTextAttribute( HANDLE hConsoleOutput,WORD wAttributes);
 一共有16种文字颜色，16种背景颜色，可以通过
 文字颜色 + 背景颜色 * 0x10 来设置控制台的颜色
-wAttributes			颜色		对应的值
-FOREGROUND_BLUE		字体颜色：蓝	1
-FOREGROUND_GREEN		字体颜色：绿	2
-FOREGROUND_RED		字体颜色：红	4
-FOREGROUND_INTENSITY	前景色高亮显示	8
-BACKGROUND_BLUE		背景颜色：蓝	16
-BACKGROUND_GREEN		背景颜色：绿	32
-BACKGROUND_RED		背景颜色：红	64
-BACKGROUND_INTENSITY	背景色高亮显示	128
-
+wAttributes			颜色			对应的值
+FOREGROUND_BLUE		text color：蓝		1
+FOREGROUND_GREEN		text color：绿		2
+FOREGROUND_RED		text color：红		4
+FOREGROUND_INTENSITY	前景色高亮显示		8
+//背景颜色其实就是上面值*0x10
+BACKGROUND_BLUE		background color：蓝	16
+BACKGROUND_GREEN		background color：绿	32
+BACKGROUND_RED		background color：红	64
+BACKGROUND_INTENSITY	背景色高亮显示		128
+//白色即用FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE，也可以直接用7表示
+一共有16种文字颜色，16种背景颜色，组合有256种
 == 彩色的Hello World! ==
 #include <stdio.h>
 #include <windows.h>
