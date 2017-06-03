4fdf017dd2b8b9362a8d811e59107401d86d2b86
Kestory <kestory@163.com>
2017-06-03 12:44:11 +0800

    tiny changes

--- C:CPP/Windows.cpp
+++ C:CPP/Windows.cpp
@@ -5,7 +5,7 @@ typedef struct _COORD {
 } COORD, *PCOORD;
 COORD 结构体定义了控制台屏幕缓冲区中字符单元格的坐标
 COORD pos;
-它的两个成员是X和Y(单位取决于函数调用)
+pos的两个成员是X和Y(单位取决于函数调用)
 (0,0) is at the top, left cell of the buffer.
 
 
@@ -27,6 +27,36 @@ DWORD 双字即为4个字节，每个字节是8位，共32位
 //A DWORD is a 32-bit unsigned integer (range: 0 through 4294967295 decimal).
 typedef unsigned long DWORD, *PDWORD, *LPDWORD;
 
+
+
+=======windows操作系统下system()函数=======
+int system(char *command);//这里就是调用windows的DOS命令，和在CMD里面使用一样
+system("pause")可以实现冻结屏幕，便于观察程序的执行结果
+system("CLS")可以实现清屏操作//windows不管大小写的呀
+~~system()调用color命令~~
+COLOR [attr]//attr - 颜色属性，由两个十六进制数字指定 -- 第一个为背景，第二个则为前景
+每个数字可以为以下任何值之一，大小写无所谓，若前景和背景颜色相同，和不设置一样
+0 = 黑	 	8 = 灰色
+1 = 蓝色	9 = 淡蓝色
+2 = 绿色	A = 淡绿色
+3 = 浅绿色 	B = 淡浅绿色
+4 = 红色	C = 淡红色
+5 = 紫色	D = 淡紫色
+6 = 黄色	E = 淡黄色
+7 = 白色	F = 亮白色
+system("color 0b");//黑底浅绿
+这里都是全局变色，且只看最后一个system("color **");
+
+==============只一行变色==============
+#include <windows.h>
+#include <stdio.h>
+HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+SetConsoleTextAttribute(hConsole, 4);
+cout << "这行是红的，下面变回白色，不过前提全是黑色背景";
+SetConsoleTextAttribute(hConsole, 7);
+
+-------SetConsoleTextAttribute 函数-------
+BOOL SetConsoleTextAttribute( HANDLE hConsoleOutput,WORD wAttributes);
 
 
 == 彩色的Hello World! ==
@@ -52,32 +82,8 @@ int main()
 {
 	SetColor();
 	std::cout << "Hello world!" << endl;
-	SetColor(40, 30);//之后都是绿低黄字
+	SetColor(40, 30);//之后都是绿底黄字
 	std::cout << "Hello world!" << endl;
 	std::cout << "Hello world!" << endl;
 	return 0;
 }
-=======windows操作系统下system()函数=======
-int system(char *command);//这里就是调用windows的DOS命令，和在CMD里面使用一样
-system("pause")可以实现冻结屏幕，便于观察程序的执行结果
-system("CLS")可以实现清屏操作//windows不管大小写的呀
-~~system()调用color命令~~
-COLOR [attr]//attr - 颜色属性，由两个十六进制数字指定 -- 第一个为背景，第二个则为前景
-每个数字可以为以下任何值之一，大小写无所谓，若前景和背景颜色相同，和不设置一样
-0 = 黑	 	8 = 灰色
-1 = 蓝色	9 = 淡蓝色
-2 = 绿色	A = 淡绿色
-3 = 浅绿色 	B = 淡浅绿色
-4 = 红色	C = 淡红色
-5 = 紫色	D = 淡紫色
-6 = 黄色	E = 淡黄色
-7 = 白色	F = 亮白色
-system("color 0b");//黑底浅绿
-这里都是全局变色，且只看最后一个system("color **");
-~~想要一行还得~~
-#include <windows.h>
-#include <stdio.h>
-HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-SetConsoleTextAttribute(hConsole, 4);
-cout << "这行是红的，下面变回白色，不过前提全是黑色背景";
-SetConsoleTextAttribute(hConsole, 7);
