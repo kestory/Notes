6794c87c5a24f72fbddb926c36db9f3d79a6ad95
Kestory <kestory@163.com>
2017-06-06 23:40:16 +0800

    goodlocality

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 git:
 	git add .
 	git status
-	git commit -m "AutoGit.sh"
+	git commit -m "goodlocality"
 	git push
---
+++ code/goodlocality.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#define NUM 1024
+#define COUNT 10
+int A[NUM][NUM];
+int main () {
+	int i, j, k;
+	for (i = 0; i < NUM; i++)			//b
+		for (j = 0; j     < NUM; j++)		//c
+			for (k = 0; k < COUNT; k++)	//a
+				A[i][j] = i + j;
+	printf("%d count computing over!\n", i * j * k);
+}
+/*
+time ./goodlocality
+abc
+0.04s user 0.00s system 86% cpu 0.053 total
+acb
+0.25s user 0.00s system 96% cpu 0.268 total
+bac
+0.04s user 0.00s system 84% cpu 0.048 total
+bca#BEST
+0.02s user 0.00s system 78% cpu 0.034 total
+cab#WORST
+0.28s user 0.01s system 98% cpu 0.292 total
+cba
+0.04s user 0.00s system 92% cpu 0.046 total
+Result changes every time, compare these relatively!
+*/
