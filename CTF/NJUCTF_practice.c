347049dbfc7cfde8cf133417683c2974bfef505b
Kestory <kestory@163.com>
2017-10-16 20:25:07 +0800

    CTF Wargame 01

--- CTF/NJUCTF_practice.c
+++ CTF/NJUCTF_practice.c
@@ -6,6 +6,16 @@ fping -ag 202.119.32.0 202.119.63.255查看存活机器
 Wargame中切换题目su即可
 su level01
 
+修改PATH环境变量
+PATH=/tmp:$PATH    让/tmp出现在PATH最前面
+
+提升权限
+chmod 777 file
+-rwxrwxrwx 任何人都有读、写、运行权限
+
+链接
+ln -s /bin/getflag01 /tmp/echo
+
 
 
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
