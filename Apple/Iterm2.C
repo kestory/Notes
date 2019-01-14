c4062d1e9f86b9d13baac324ad456bfc652f8b18
kestory <kestory@163.com>
2019-01-14 16:35:15 +0800

    Terminal Cheatsheet

--- Apple/Iterm2.C
+++ Apple/Iterm2.C
@@ -2,20 +2,19 @@
 选中即复制
 
 ⌘ + d：左右分屏
-⇧⌘ + d：上下分屏
+⇧ + ⌘ + d：上下分屏
 
 ⌘ + ; ：自动补全历史记录
 ⌘ + ⇧ + h：剪贴板历史
-⌥⌘ + b：历史回放
+⌥ + ⌘ + b：历史回放
 
 ⌘ + / ：显示光标位置
-⌘ + ⌥ + e：全屏展示所有的 tab，且可以搜索
+⌥ + ⌘ + e：全屏展示所有的 tab，且可以搜索
 
-⌃ + l：清屏
 
-终端常用的快捷键：
-ctrl+shift+c: 复制
-ctrl+shift+v: 粘贴
+Terminal Cheatsheet
+Ctrl+Shift+c: 复制
+Ctrl+Shift+v: 粘贴
 
 Ctrl + w        向左删一个单词
 
@@ -29,5 +28,5 @@ Ctrl + e        光标移动到行尾（End of line）
 Ctrl + l        清屏（相当于 clear 命令）
 
 Ctrl + r        显示：号提示，根据用户输入查找相关历史命令（reverse-i-search）
-Alt  + .         粘帖最后一次命令最后的参数（通常用于mkdir xx 后, cd配合着alt+.）//可以连续使用，用一次的话相当于 !$
+Alt  + .        粘帖最后一次命令最后的参数（通常用于mkdir xx 后, cd配合着alt+.）//可以连续使用，用一次的话相当于 !$
 
--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 git:
 	git add .
 	git status
-	git commit -m "编译原理第一章的块作用域"
+	git commit -m "Terminal Cheatsheet"
 	git push
