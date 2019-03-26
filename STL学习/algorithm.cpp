489a49bdb5d6418afbf18694627bfc6d017fe14e
kestory <kestory@163.com>
2019-03-26 10:15:46 +0800

    algorithm_reverse

--- STL学习/algorithm.cpp
+++ STL学习/algorithm.cpp
@@ -1,6 +1,6 @@
 #include <algorithm>
 
-remove && remove_if
+------- remove && remove_if -------
 ForwardIt remove( ForwardIt first, ForwardIt last, const T& value );
 ForwardIt remove_if( ForwardIt first, ForwardIt last, UnaryPredicate p );
 这两个方法的前两个参数相同，第三个参数不同：remove的第三个参数是常量；remove_if的第三个参数是一个过滤条件
@@ -8,7 +8,8 @@ ForwardIt remove_if( ForwardIt first, ForwardIt last, UnaryPredicate p );
 str1.erase(remove(str1.begin(), str1.end(), ' '), str1.end());
 str2.erase(remove_if(str2.begin(), str2.end(), [](unsigned char x) {return isspace(x);}),
 
-Heap using STL C++
+
+------- Heap using STL C++ -------
 int myints[] = {20, 30, 40, 25, 15};
 vector<int> v1(myints, myints + 5);
 make_heap(v1.begin(), v1.end());
@@ -25,3 +26,14 @@ sort_heap(v1.begin(), v1.end());//After this operation, the container is no long
 for (int &x : v1)
 	cout << x << " ";
 cout << endl;
+
+------- reverse -------
+string s = "1234";
+reverse(s.begin(), s.end());
+cout << s << endl;
+
+int n[] = {1, 2, 3, 4, 5};
+vector<int> v(n, n + 5);
+reverse(v.begin(), v.end());
+for (int &x : v)
+	cout << x << " ";
